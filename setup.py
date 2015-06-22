13bbe226d91d994304da651ab175cf8e52c1dbae
Evan Tschuy <tschuy@osuosl.org>
2015-06-22 15:48:51 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+dependencies = [
+    'Django==1.7.4',
+]
+
+setup(
+    name='python-test-apps',
+    version='1.0.0',
+    install_requires=dependencies,
+    author='OSU Open Source Lab',
+    author_email='support@osuosl.org',
+    packages=find_packages(),
+    url='https://github.com/osuosl/python-test-apps',
+    license='Apache 2',
+    description='',
+    long_description=open('README.md').read()
+)
